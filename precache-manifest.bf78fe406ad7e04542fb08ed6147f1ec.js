77c26171948f95c53d1ab139717b2ea2c4a2ba1c
Кудрявцев Дмитрий Вальтерович <DVKudryavtsev@alfabank.ru>
2018-12-15 19:16:36 +0300

    Updates

---
+++ precache-manifest.bf78fe406ad7e04542fb08ed6147f1ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ede9bd6fa0ef6d3b2ffe",
+    "url": "/piano-visualisation/static/js/runtime~main.ede9bd6f.js"
+  },
+  {
+    "revision": "09e2d3014f7ff60ee9b8",
+    "url": "/piano-visualisation/static/js/main.09e2d301.chunk.js"
+  },
+  {
+    "revision": "74bd6d47c72a3144fcf2",
+    "url": "/piano-visualisation/static/js/1.74bd6d47.chunk.js"
+  },
+  {
+    "revision": "09e2d3014f7ff60ee9b8",
+    "url": "/piano-visualisation/static/css/main.01ac9cdc.chunk.css"
+  },
+  {
+    "revision": "eb8918b88fe5b7dd25c70e82831603a5",
+    "url": "/piano-visualisation/index.html"
+  }
+];
--- src/constants/types.js
+++
@@ -1,9 +0,0 @@
-export default {
-    1: 'whole',
-    2: 'half',
-    4: 'quarter',
-    8: 'eighth',
-    16: '16th',
-    32: '32nd',
-    64: 'th',
-}
